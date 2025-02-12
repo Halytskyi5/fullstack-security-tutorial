e084a4c093a5c6bb04834f20778adcd253b0470d
Halytskyi5 <volodyaforschool@gmail.com>
2025-02-12 18:37:16 +0200

    add method login()

---
+++ backend/src/main/java/com/example/backend/services/UserService.java
@@ -0,0 +1,32 @@
+package com.example.backend.services;
+
+import com.example.backend.dto.CredentialsDto;
+import com.example.backend.dto.UserDto;
+import com.example.backend.entities.User;
+import com.example.backend.exceptions.AppException;
+import com.example.backend.mappers.UserMapper;
+import com.example.backend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.nio.CharBuffer;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserMapper userMapper;
+
+    public UserDto login(CredentialsDto credentialsDto) {
+        User user = userRepository.findByLogin(credentialsDto.login())
+                .orElseThrow(() -> new AppException("Unknown user", HttpStatus.NOT_FOUND));
+        if(passwordEncoder.matches(CharBuffer.wrap(credentialsDto.password()),
+                user.getPassword())) {
+            return userMapper.toUserDto(user);
+        }
+        throw new AppException("Invalid password", HttpStatus.BAD_REQUEST);
+    }
+}
