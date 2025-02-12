098487303334e7b5ded56755fac304ddafc6100f
Halytskyi5 <volodyaforschool@gmail.com>
2025-02-12 14:17:40 +0200

    add function onSubmitLoginEvent and create post request

--- frontend/src/app/content/content.component.ts
+++ frontend/src/app/content/content.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import {WelcomeContentComponent} from '../welcome-content/welcome-content.component';
 import {LoginFormComponent} from '../login-form/login-form.component';
+import {ServerService} from '../server.service';
+import {User} from '../models/user';
 
 @Component({
   selector: 'app-content',
   imports: [
     WelcomeContentComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    ServerService
   ],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss',
   standalone: true
 })
 export class ContentComponent {
+  constructor(private serverService : ServerService) {
+  }
 
-}
+  onLogin(user : User) : void {
+    this.serverService.postLogin(user);
+  }
+ }
--- frontend/src/app/server.service.ts
+++ frontend/src/app/server.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {User} from './models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,9 @@ export class ServerService {
   constructor(public http : HttpClient) { }
   getMessages() : Observable<any> {
     return this.http.get<any>(`${this.url}/messages`);
+  }
+  postLogin(user : User) {
+    return this.http.post<any>(`${this.url}/login`, user);
   }
 
 }
