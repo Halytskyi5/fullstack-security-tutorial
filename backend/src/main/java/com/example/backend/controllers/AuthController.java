babf3ff2111fdc1cdb0092ea6405890187e88610
Halytskyi5 <volodyaforschool@gmail.com>
2025-02-12 18:42:02 +0200

    add post request for login

---
+++ backend/src/main/java/com/example/backend/controllers/AuthController.java
@@ -0,0 +1,24 @@
+package com.example.backend.controllers;
+
+import com.example.backend.dto.CredentialsDto;
+import com.example.backend.dto.UserDto;
+import com.example.backend.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<UserDto> login(@RequestBody CredentialsDto credentialsDto) {
+        UserDto user = userService.login(credentialsDto);
+        return ResponseEntity.ok(user);
+    }
+}
