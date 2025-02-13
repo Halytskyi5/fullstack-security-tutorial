11c6d1ca470ea5e123e133f0ce6ed747effcf260
Halytskyi5 <volodyaforschool@gmail.com>
2025-02-13 13:02:07 +0200

    add exception handler

--- frontend/src/app/content/content.component.ts
+++ frontend/src/app/content/content.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {WelcomeContentComponent} from '../welcome-content/welcome-content.component';
 import {LoginFormComponent} from '../login-form/login-form.component';
 import {ServerService} from '../server.service';
 import {User} from '../models/user';
+import {log} from '@angular-devkit/build-angular/src/builders/ssr-dev-server';
+import {tap} from 'rxjs';
 
 @Component({
   selector: 'app-content',
@@ -15,11 +17,22 @@ import {User} from '../models/user';
   standalone: true
 })
 export class ContentComponent {
-  constructor(private serverService : ServerService) {
+  constructor(private serverService: ServerService) {
   }
 
-  onLogin(user : User) : void {
-    this.serverService.postLogin(user);
+  onLogin(user: User): void {
     console.log(user)
+    this.serverService.postLogin(user).subscribe({
+      next: (response) => {
+        console.log('res ', response)
+      },
+      error: (error) => {
+        console.log(error.error.message)
+      },
+      complete: () => {
+        console.log('finish')
+      }
+    });
   }
- }
+
+}
